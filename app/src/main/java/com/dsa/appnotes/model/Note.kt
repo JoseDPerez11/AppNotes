b2440f004e2d958bf40086c2e279db5a8b686ff5
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2024-03-01 00:32:53 -0500

    adding NoteDao and NoteDatabase

---
+++ app/src/main/java/com/dsa/appnotes/model/Note.kt
@@ -0,0 +1,15 @@
+package com.dsa.appnotes.model
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity(tableName = "notes")
+@Parcelize
+data class Note(
+    @PrimaryKey
+    val id: Int,
+    val noteTitle: String,
+    val noteDesc: String
+) : Parcelable
