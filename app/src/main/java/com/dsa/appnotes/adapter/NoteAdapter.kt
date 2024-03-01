16e025059e6502420ec07d798fee25f8696a9ea8
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2024-03-01 18:54:47 -0500

    adding note_layout for recyclerview and NoteAdapter

---
+++ app/src/main/java/com/dsa/appnotes/adapter/NoteAdapter.kt
@@ -0,0 +1,52 @@
+package com.dsa.appnotes.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.dsa.appnotes.databinding.NoteLayoutBinding
+import com.dsa.appnotes.model.Note
+
+class NoteAdapter: RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    class NoteViewHolder(val itemBinding: NoteLayoutBinding): RecyclerView.ViewHolder(itemBinding.root)
+
+
+    private val differCallback = object:DiffUtil.ItemCallback<Note>() {
+        // Se sobrescribe el método areItemsTheSame que compara si los elementos antiguos y nuevos son los mismos.
+        override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem.id == newItem.id &&
+                    oldItem.noteTitle == newItem.noteTitle &&
+                    oldItem.noteDesc == newItem.noteDesc
+        }
+
+        // Se sobrescribe el método areContentsTheSame que compara si los contenidos de los elementos antiguos y nuevos son iguales.
+        override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        return NoteViewHolder(
+            NoteLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val currentNote = differ.currentList[position]
+
+        holder.itemBinding.noteTitle.text = currentNote.noteTitle
+        holder.itemBinding.noteDesc.text = currentNote.noteDesc
+
+        
+    }
+
+}
