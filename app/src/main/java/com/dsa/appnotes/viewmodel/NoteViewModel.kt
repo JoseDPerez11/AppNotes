8bf65189fdbc080e2fcc5974a9f9f6d47b0cc850
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2024-03-01 20:10:08 -0500

    implemented ViewModel

---
+++ app/src/main/java/com/dsa/appnotes/viewmodel/NoteViewModel.kt
@@ -0,0 +1,31 @@
+package com.dsa.appnotes.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.dsa.appnotes.model.Note
+import com.dsa.appnotes.repository.NoteRepository
+import kotlinx.coroutines.launch
+
+class NoteViewModel(app: Application, private val noteRepository: NoteRepository): AndroidViewModel(app) {
+
+    fun addNote(note: Note) =
+        viewModelScope.launch {
+            noteRepository.insertNote(note)
+        }
+
+    fun deleteNote(note: Note) =
+        viewModelScope.launch {
+            noteRepository.deleteNote(note)
+        }
+
+    fun editNote(note: Note) =
+        viewModelScope.launch {
+            noteRepository.updateNote(note)
+        }
+
+    fun getAllNotes() = noteRepository.getAllNotes()
+
+    fun searchNotes(query: String?) = noteRepository.searchNotes(query)
+
+}
