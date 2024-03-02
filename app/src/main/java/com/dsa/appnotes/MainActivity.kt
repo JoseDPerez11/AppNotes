6bce52b5f26c8e706b7fbfa004a7617cc7c986fc
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2024-03-02 00:00:44 -0500

    implemented homefragments

--- app/src/main/java/com/dsa/appnotes/MainActivity.kt
+++ app/src/main/java/com/dsa/appnotes/MainActivity.kt
@@ -2,10 +2,27 @@ package com.dsa.appnotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.dsa.appnotes.database.NoteDatabase
+import com.dsa.appnotes.repository.NoteRepository
+import com.dsa.appnotes.viewmodel.NoteViewModel
+import com.dsa.appnotes.viewmodel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupViewModel()
+    }
+
+    private fun setupViewModel() {
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
     }
 }
